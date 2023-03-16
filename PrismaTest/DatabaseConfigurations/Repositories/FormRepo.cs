ed01cd1a8f6f12b50e8ad14bfa0c07e6271061f2
Kosmas_Stamos <kossmasstamoss@gmail.com>
2023-03-16 10:45:29 +0200

    Forms Repo Done Also changed the connection string

--- PrismaTest/DatabaseConfigurations/Repositories/FormRepo.cs
+++ PrismaTest/DatabaseConfigurations/Repositories/FormRepo.cs
@@ -11,9 +11,7 @@ using System.Threading.Tasks;
 namespace DatabaseConfigurations.Repositories
 {
     public class FormRepo : IEntityRepo<Forms>
-    {//TODO: Form Repo crud
-
-
+    {
         private readonly IConfiguration _configuration;
         //injection
         public FormRepo(IConfiguration configuration)
@@ -23,12 +21,51 @@ namespace DatabaseConfigurations.Repositories
 
         public void Add(Forms entity)
         {
-            throw new NotImplementedException();
-        }
+            using (var connection = new SqlConnection(_configuration.GetConnectionString("SQLConnection")))
+            {
+                try
+                {
+                    connection.Open();
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                }
+                //created adapter and command to pass to the adapter
+                SqlDataAdapter formAdapter = new SqlDataAdapter();
+                SqlCommand command = new SqlCommand(
+                    "Insert Into Forms(Title,Description,DateOfCreation,LastUpdated)" +
+                    "Values(@title,@Description,@DateOfCreation,@LastUpdated)", connection);
+                formAdapter.InsertCommand = command; //I am unsure about how I would use the adapter here tbh
 
+                command.Parameters.AddWithValue("@Title", entity.Title);
+                command.Parameters.AddWithValue("@Description", entity.Description);
+                command.Parameters.AddWithValue("@DateOfCreation", entity.DateOfCreation);
+                command.Parameters.AddWithValue("@LastUpdated", entity.LastUpdated);
+
+                command.ExecuteNonQuery(); // number of rows affected should be 1
+            }
+        }
         public void Delete(int ID)
         {
-            throw new NotImplementedException();
+            using (var connection = new SqlConnection(_configuration.GetConnectionString("SQLConnection")))
+            {
+                try
+                {
+                    connection.Open();
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                }
+
+                SqlCommand command = new SqlCommand(
+                    "Delete From Forms Where Id = @Id", connection);
+
+                command.Parameters.AddWithValue("@Id", ID);
+
+                command.ExecuteNonQuery();
+            }
         }
 
         public IList<Forms> GetAll()
@@ -45,7 +82,7 @@ namespace DatabaseConfigurations.Repositories
                 {
                     throw ex;
                 }
-                
+                //created adapter and command to pass to the adapter
                 SqlDataAdapter formAdapter = new SqlDataAdapter();
                 SqlCommand command = new SqlCommand(
                     "Select * Forms;", connection);
@@ -63,24 +100,74 @@ namespace DatabaseConfigurations.Repositories
                         form.Description = Convert.ToString((formTable.Rows[i]["Description"]));
                         form.DateOfCreation = Convert.ToDateTime((formTable.Rows[i]["DateOfCreation"]));
                         form.LastUpdated = Convert.ToDateTime((formTable.Rows[i]["LastUpdated"]));
-                    
+
                         FormList.Add(form);
                     }
                 }
             }
-
-
             return FormList;
         }
 
         public Forms? GetById(int ID)
         {
-            throw new NotImplementedException();
-        }
+            Forms form = null;
+            using (var connection = new SqlConnection(_configuration.GetConnectionString("SQLConnection")))
+            {
+                try
+                {
+                    connection.Open();
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                }
 
+                SqlDataAdapter formAdapter = new SqlDataAdapter();
+                SqlCommand command = new SqlCommand("Select * From Forms Where Id=@Id", connection);
+                command.Parameters.AddWithValue("@Id", ID);
+                formAdapter.SelectCommand = command;
+
+                DataTable formTable = new DataTable("Forms");
+                formAdapter.Fill(formTable);
+
+                if (formTable.Rows.Count > 0)
+                {
+                    form = new Forms();
+                    form.Id = Convert.ToInt32(formTable.Rows[0]["Id"]);
+                    form.Title = Convert.ToString(formTable.Rows[0]["Title"]);
+                    form.Description = Convert.ToString((formTable.Rows[0]["Description"]));
+                    form.DateOfCreation = Convert.ToDateTime((formTable.Rows[0]["DateOfCreation"]));
+                    form.LastUpdated = Convert.ToDateTime((formTable.Rows[0]["LastUpdated"]));
+                }
+                return form;
+            }
+        }
         public void Update(int ID, Forms entity)
         {
-            throw new NotImplementedException();
+            using (var connection = new SqlConnection(_configuration.GetConnectionString("SQLConnection")))
+            {
+                try
+                {
+                    connection.Open();
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                }
+                //created adapter and command to pass to the adapter
+                SqlDataAdapter formAdapter = new SqlDataAdapter();
+                SqlCommand command = new SqlCommand(
+                    "UPDATE Forms SET Title=@Title, Description=@Description, " +
+                    "DateOfCreation=@DateOfCreation, LastUpdated=@LastUpdated " +
+                    "WHERE Id=@Id", connection);
+
+                command.Parameters.AddWithValue("@Title", entity.Title);
+                command.Parameters.AddWithValue("@Description", entity.Description);
+                command.Parameters.AddWithValue("@DateOfCreation", entity.DateOfCreation);
+                command.Parameters.AddWithValue("@LastUpdated", entity.LastUpdated);
+
+                command.ExecuteNonQuery();
+            }
         }
     }
 }
